8cd863cc774659cb6ec40fe0fbde3f2175916f3b
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2023-01-17 19:30:43 +0300

    HomeWork

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Write("Введите значения m и n в строку через пробел и нажмите ввод ");
+string[] num = Console.ReadLine().Split(' ');
+int m = Convert.ToInt32(num[0]);
+int n = Convert.ToInt32(num[1]);
+Console.WriteLine($" Количество строк = {m}; Количество столбцов = {n}");
+
+double[,] arr = new double[m, n];
+
+Random random = new Random(100);
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        arr[i, j] = random.NextDouble() * 10; // получаю случайное вещественное число в диапазоне от 0 до 1.
+                                              // умножаю результат на 10 чтобы получить целую часть.
+        Console.Write("{0,5:F1}", arr[i, j]);
+    }
+    Console.WriteLine();
+}
+
+
---
+++ HomeWork5/Program.cs
@@ -0,0 +1,64 @@
+﻿// задача 2 HARD необязательная. Считается за четыре обязательных ) то есть три этих и одна с будущего семинара
+
+// 1. Сгенерировать массив случайных целых чисел размерностью m*n (размерность вводим с клавиатуры) , причем чтоб количество элементов было четное. 
+
+// 2. Вывести на экран красивенько таблицей. 
+
+// 3. Перемешать случайным образом элементы массива, причем чтобы каждый элемент гарантированно и только один раз 
+// переместился на другое место и выполнить перемешивание за m*n / 2 итераций. 
+
+// 4. То есть если массив три на четыре, то надо выполнить за 6 итераций. И далее в конце опять вывести на экран как таблицу.
+
+var m = 0;
+var n = 0;
+do
+{
+    Console.Write("Введите значения m и n ( m*n должно быть четным числом) в строку через пробел и нажмите ввод ");
+    string[] num = Console.ReadLine().Split(' ');
+    m = Convert.ToInt32(num[0]);
+    n = Convert.ToInt32(num[1]);
+}
+while ((m * n) % 2 != 0);
+
+
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+MixArray(array, m, n);
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(1, 10);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3} \t");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void MixArray(int[,] array, int m, int n)
+{
+    var rnd = new Random();
+    for (var i = 0; i < m * n / 2; i++)
+    {
+        int r1 = rnd.Next(0, array.GetLength(0)),
+            r2 = rnd.Next(0, array.GetLength(0)),
+            c1 = rnd.Next(0, array.GetLength(1)),
+            c2 = rnd.Next(0, array.GetLength(1));
+        int temp = array[r1, c1];
+        array[r1, c1] = array[r2, c2];
+        array[r2, c2] = temp;
+    }
+    PrintArray(array);
+}
+
+
